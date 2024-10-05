0bf3e9de89997433da7442cc18a3dd7a56a95f8c
Ayush Kumar <75977554+unstoppableayush@users.noreply.github.com>
2024-10-05 07:34:47 +0530

    Update contributors.js

--- contributors.js
+++ contributors.js
@@ -2,7 +2,8 @@ import dennis from "./contributors/dennis.json";
 import kushboo from "./contributors/kushboo.json";
 import manali from "./contributors/manali.json";
 import meghana from "./contributors/meghana.json";
+import ayush from "./contributors/ayush.json";
 
-const contributors = [dennis, kushboo, manali, meghana];
+const contributors = [dennis, kushboo, manali, meghana, ayush];
 
 export default contributors;
