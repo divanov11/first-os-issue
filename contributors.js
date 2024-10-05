8e29275bf522bb8a3d66febdbe7dc83af88a36ab
Kalvin Calimag <calimag.kalvin.d@gmail.com>
2024-10-05 10:39:04 +0800

    Update contributors.js

--- contributors.js
+++ contributors.js
@@ -2,7 +2,8 @@ import dennis from "./contributors/dennis.json";
 import kushboo from "./contributors/kushboo.json";
 import manali from "./contributors/manali.json";
 import meghana from "./contributors/meghana.json";
+import kalvin from "./contributors/kalvin.json";
 
-const contributors = [dennis, kushboo, manali, meghana];
+const contributors = [dennis, kushboo, manali, meghana, kalvin];
 
 export default contributors;
