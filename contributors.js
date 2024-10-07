cd33845641fd7995720c6185fc7f12e49211d7a4
divanov11 <ivanovsin11@gmail.com>
2024-10-07 09:49:00 -0700

    -

--- contributors.js
+++ contributors.js
@@ -4,9 +4,18 @@ import manali from "./contributors/manali.json";
 import meghana from "./contributors/meghana.json";
 import kalvin from "./contributors/kalvin.json";
 import ayush from "./contributors/ayush.json";
-import Usman_Pervez from "./contributors/Usman_Pervez.json"
-import shamsul from "./contributors/shamsul.json"
+import usman_Pervez from "./contributors/usman_pervez.json";
+import shamsul from "./contributors/shamsul.json";
 
-const contributors = [dennis, kushboo, manali, meghana, ayush, kalvin, Usman_Pervez, shamsul];
+const contributors = [
+    dennis,
+    kushboo,
+    manali,
+    meghana,
+    ayush,
+    kalvin,
+    usman_Pervez,
+    shamsul,
+];
 
 export default contributors;
