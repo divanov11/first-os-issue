b12475485179dd431f59b7ca7aa2b4791522c31e
Dennis Ivy <33843378+divanov11@users.noreply.github.com>
2024-10-07 09:14:22 -0700

    Update contributors.js

--- contributors.js
+++ contributors.js
@@ -4,7 +4,7 @@ import manali from "./contributors/manali.json";
 import meghana from "./contributors/meghana.json";
 import kalvin from "./contributors/kalvin.json";
 import ayush from "./contributors/ayush.json";
-import usman_pervez "./contributors/Usman_Pervez.json"
+import usman_pervez from "./contributors/Usman_Pervez.json"
 import shamsul from "./contributors/shamsul.json"
 
 const contributors = [dennis, kushboo, manali, meghana, ayush, kalvin, usman_pervez, shamsul];
